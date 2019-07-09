9696b68f6a3309f7ccaccdca885eb078ba7db2c0
Vinicius Mussato <vinicius@mbp-de-vinicius.localdomain>
2019-07-09 16:01:54 -0300

    created card list component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
 
+import { CardList } from './components/card-list/card-list.component'; 
 class App extends Component {
   constructor() {
     super();
@@ -18,10 +18,12 @@ class App extends Component {
 
   render() {
     return(
-    <div className="App">
-      {
-        this.state.monsters.map( monster => <h1 key={monster.id}> {monster.name}</h1>)
-      }
+    <div className='App'>
+      <CardList cardName='Vinicius'>
+        {this.state.monsters.map( monster => (
+          <h1 key={monster.id}> {monster.name}</h1>
+        ))}
+      </CardList>  
     </div>
     )
   }
