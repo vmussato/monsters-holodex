7742af51f161bb2125bc6baad2d5cb6ff9767db3
Vinicius Mussato <mtzcpd1486>
2019-07-10 20:12:19 -0300

    finished monster filter

--- src/App.js
+++ src/App.js
@@ -18,10 +18,16 @@ class App extends Component {
   }
 
   render() {
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+
     return(
     <div className='App'>
       <input type='search' placeholder='search-monsters' onChange={e => this.setState({searchField: e.target.value})} />
-      <CardList monsters={this.state.monsters} /> 
+      <CardList monsters={filteredMonsters} /> 
     </div>
     )
   }
