558323baaded55054a576c593f9def9f71f6ebf4
Vinicius Mussato <mtzcpd1486>
2019-07-12 13:46:28 -0300

    create method handleChange

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ class App extends Component {
     .then(users => this.setState({monsters: users}));
   }
 
+  handleChange = e => {
+    this.setState({searchField: e.target.value});
+  }
+
   render() {
 
     const { monsters, searchField } = this.state;
@@ -32,7 +36,7 @@ class App extends Component {
     <div className='App'>
       <SearchBox 
          placeholder='search-monsters'
-         handleChange={e => this.setState({searchField: e.target.value})}
+         handleChange={this.handleChange}
       />
       <CardList monsters={filteredMonsters} /> 
     </div>
