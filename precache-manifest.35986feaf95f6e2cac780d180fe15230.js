df80101ae8e3ee08fccb538c61f3f29fa13246e4
Vinicius Mussato <mtzcpd1486>
2019-07-12 15:26:17 -0300

    Updates

---
+++ precache-manifest.35986feaf95f6e2cac780d180fe15230.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb69ea385df808d1d4148d7321a1c878",
+    "url": "/monsters-holodex/index.html"
+  },
+  {
+    "revision": "52aa9a70f1638f5ba0e0",
+    "url": "/monsters-holodex/static/css/main.df2c6aa1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-holodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "52aa9a70f1638f5ba0e0",
+    "url": "/monsters-holodex/static/js/main.bea5ed9e.chunk.js"
+  },
+  {
+    "revision": "9240fbce318392ebb0f1",
+    "url": "/monsters-holodex/static/js/runtime~main.bf0e321f.js"
+  }
+]);
